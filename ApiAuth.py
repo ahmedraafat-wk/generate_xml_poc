8e1e12ca64e2f8483b7cf2862bb9b55814fad298
Ahmed Raafat <ahmed.raafat@workiva.com>
2022-08-25 11:52:23 +0200

    Resturcute the spreadsheets process

---
+++ ApiAuth.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+import requests
+import json
+
+
+class ApiAuth:
+    def __init__(self, url):
+        self._url = url
+        self._headers = {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+    
+    def authenticate(self, client_id: str, client_secret: str):
+        '''
+        This function authenticate the user by the client id & secret with the Workiva platform API
+        
+        :param client_id: a string value that represent the client ID
+        :param client_secret: a string value that represent the client secret
+        
+        :return assign the retrieved auth token to the class access_token var.
+        '''
+
+        data = 'client_id=' + client_id + '&client_secret=' + \
+            client_secret + '&grant_type=client_credentials'
+        
+        response = requests.post(
+            self._url + '/iam/v1/oauth2/token', data=data, headers=self._headers)
+        token_data = json.loads(response.text)
+        return token_data['access_token']
--- SpreadsheetsApi.py
+++ SpreadsheetsApi.py
@@ -1,27 +1,12 @@
 #!/usr/local/bin/python3
 import requests
 import json
-import os
-from dotenv import load_dotenv
-from pprint import pp, pprint
+from pprint import pprint
 
-
-# Load the environment variables
-load_dotenv()
-
-# AUTH_URL = os.getenv('AUTH_URL')
-# SS_API_URL = os.getenv('SS_API_URL')
-API_URL = os.getenv('API_URL')
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
-SHEET_ID = os.getenv('SHEET_ID')
-
-
-class PlatformAPI:
-    def __init__(self, url: str):
+class SpreadsheetsApi:
+    def __init__(self, url, access_token):
         self._url = url
-        self._access_token = None
+        self._access_token = access_token
 
     def _get_headers(self):
         '''
@@ -80,21 +65,6 @@ class PlatformAPI:
         resp = requests.put(url, data=json.dumps(data),
                             headers=self._get_headers())
         return resp.status_code
-
-    def authenticate(self, client_id: str, client_secret: str):
-        '''
-        This function authenticate the user by the client id & secret with the Workiva platform API
-        
-        :param client_id: a string value that represent the client ID
-        :param client_secret: a string value that represent the client secret
-        
-        :return assign the retrieved auth token to the class access_token var.
-        '''
-
-        data = 'client_id=' + client_id + '&client_secret=' + \
-            client_secret + '&grant_type=client_credentials'
-        self._access_token = self._post(
-            '/iam/v1/oauth2/token', data, {}, {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'})['access_token']
 
     def get_sheet_data(self, spreadsheet_id: str, sheet_id: str):
         '''
@@ -161,38 +131,3 @@ class PlatformAPI:
         sheet_id = self._post(path, {}, body, self._get_headers())[
             'data']['id']
         return sheet_id
-
-
-def get_sheet_values():
-    return [
-        [
-            "FunduszInwestycyjny",
-            "No type specified",
-            "Struktura sprawozdania funduszu inwestycyjnego",
-        ],
-        ["NotyObjasniajaceFI", "No type specified", "Noty objasniaj\xc4\x85ce"],
-        [
-            "NotaObjasniajaca",
-            "dtsf:TInformacjaDodatkowa",
-            "Nota objasniaj\xc4\x85ca",
-        ],
-        ["InformacjaDodatkowaFI", "No type specified", "Informacja dodatkowa"],
-        [
-            "InformacjaDodatkowa",
-            "dtsf:TInformacjaDodatkowa",
-            "Informacja dodatkowa",
-        ]
-    ]
-
-
-def main():
-    api = PlatformAPI(API_URL)
-    api.authenticate(CLIENT_ID, CLIENT_SECRET)
-    spreadsheet_id = api.create_spreadsheet('Test From Local 5')
-    sheet_id = api.create_sheet(spreadsheet_id, 'Schema Sheet')
-    api.update_range(spreadsheet_id, sheet_id, ':',
-                     {"values": get_sheet_values()})
-
-
-if __name__ == "__main__":
-    main()
